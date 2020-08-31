b138272eb0f1f40d9f4be16fb7dcb6c3a528554e
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-08-31 18:21:44 +0530

    Update E2eDLrec_cfg.py

--- E2eDLrec/python/E2eDLrec_cfg.py
+++ E2eDLrec/python/E2eDLrec_cfg.py
@@ -48,7 +48,7 @@ process.load("Configuration.StandardSequences.Reconstruction_cff")
 process.GlobalTag.globaltag = cms.string('80X_dataRun2_HLT_v12')
 process.es_prefer_GlobalTag = cms.ESPrefer('PoolDBESSource','GlobalTag')
 
-process.maxEvents = cms.untracked.PSet( input = options.maxEvents ) #options.maxEvents
+process.maxEvents = options.maxEvents #cms.untracked.PSet( input = cms.int(5) )
 process.maxEvents = options.maxEvents
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
