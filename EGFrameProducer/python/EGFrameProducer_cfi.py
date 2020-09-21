730fc2f5eb637828b867722b3759097c9fa07a9e
Michael <michael.andrews@cern.ch>
2020-09-21 02:21:22 +0200

    Reimplement EGFrameProducer as separate plugin package.

---
+++ EGFrameProducer/python/EGFrameProducer_cfi.py
@@ -0,0 +1,10 @@
+import FWCore.ParameterSet.Config as cms
+
+EGInference = cms.EDProducer('EGFrameProducer'
+    , photonCollection = cms.InputTag('gedPhotons')
+    , reducedEBRecHitCollection = cms.InputTag('reducedEcalRecHitsEB')
+    , EGModelName = cms.string("e_vs_ph_model.pb")
+    , runInference = cms.bool(True)
+    , doEBenergy = cms.bool(True)
+    , EBEnergy = cms.InputTag('ProducerFrames','EBenergy')
+    )
